4a564f39dda85dd50e6e8974733824a26c8904b1
DevSneha01 <snehaghoshsg01@gmail.com>
2022-08-05 22:58:29 +0530

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What was Meta Platforms Inc formerly known as?",
+        answer: "Facebook",
+        options: [
+            "Instagram",
+            "Facebook",
+            "whatsapp"
+        ]
+
+    },
+    {
+        numb: 2,
+        question: "Which English city is known as the Steel City?",
+        answer: "Sheffield",
+        options: [
+            "Mumbai",
+            "Austin",
+            "Sheffield"
+        ]
+
+    },
+    {
+        numb: 3,
+        question: "Which former British colony was given back to China in 1997?",
+        answer: "Hong Kong",
+        options: [
+            "Taiwan",
+            "Hong Kong",
+            "Xinjiang"
+        ]
+
+    },
+    {
+        numb: 4,
+        question: "Haematology is the study of what?",
+        answer: "The blood",
+        options: [
+            "The tissue",
+            "The cell",
+            "The blood"
+        ]
+
+    },
+    {
+        numb: 5,
+        question: "What is the main ingredient in guacamole?",
+        answer: "Avocado",
+        options: [
+            "Avocado",
+            "Apple",
+            "Pineapple"
+        ]
+
+    },
+    {
+        numb: 6,
+        question: "Rojo is the Spanish word for which colour?",
+        answer: "Red",
+        options: [
+            "Yellow",
+            "Red",
+            "Blue"
+        ]
+
+    },
+    {
+        numb: 7,
+        question: "Which is the only planet in our solar system not named after a Roman or Greek god?",
+        answer: "Earth",
+        options: [
+            "Jupiter",
+            "Mercury",
+            "Earth"
+        ]
+
+    },
+    {
+        numb: 8,
+        question: "In which sport is the Davis Cup contested?",
+        answer: "Tennis",
+        options: [
+            "Badminton",
+            "Glof",
+            "Tennis"
+        ]
+
+    },
+    {
+        numb: 9,
+        question: "How many wisdom teeth does the average adult have?",
+        answer: "Four",
+        options: [
+            "Five",
+            "Four",
+            "Eight"
+        ]
+
+    },
+    {
+        numb: 10,
+        question: "How many lives is a cat said to have?",
+        answer: "9",
+        options: [
+            "7",
+            "9",
+            "4"
+        ]
+
+    },
+];
